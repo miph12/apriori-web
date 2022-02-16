ea53e13b91ed120984902f8944ae3642fedfbc38
miftaharfn <miftahrebel@gmail.com>
2022-02-16 19:35:40 +0700

    apriori web

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                <span class="blue bolder">Data Mining</span>
+                Assalafi &copy; 2021 | Distributed by <a target="_blank" rel="noopener noreferrer">Miftahul Arifin</a>
+            </span>
+        </div>
+    </div>
+</div>
---
+++ home.php
@@ -0,0 +1,38 @@
+<div class="main-content">
+    <div class="main-content-inner">
+        <div class="page-content">
+            <div class="page-header">
+                <h1>
+                    Halaman Utama
+                </h1>
+            </div><!-- /.page-header -->
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- PAGE CONTENT BEGINS -->
+
+
+                    <div class="row">
+                        <center>
+                            <h1>
+                                Analisis Data Mining 
+                                <br>
+                                Untuk Menentukan Aturan Asosiasi Penjualan Menggunakan Algoritma Apriori dan FP Growth
+                            </h1>
+                            <p>
+                            <h2>
+                                (Studi Kasus: Minimarket Assalafi)
+                            </h2>
+                            </p>
+                        </center>
+
+                    </div><!-- /.row -->
+
+                    <div class="hr hr32 hr-dotted"></div>
+
+                    <!-- PAGE CONTENT ENDS -->
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.page-content -->
+    </div>
+</div><!-- /.main-content -->
